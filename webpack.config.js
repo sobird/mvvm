010686a389dce86446abe72c698aea75bcb0c6cb
sobird <i@sobird.me>
2019-07-03 15:34:44 +0800

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/**
+ * webpack.config
+ * 
+ * @see https://webpack.js.org/guides/getting-started/
+ * 
+ * @author  Yang,junlong at 2019-07-03 15:30:48 build.
+ * @version $Id$
+ */
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+  	filename: 'mvvm.js'
+  }
+};
