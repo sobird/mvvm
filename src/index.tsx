c936af59f5e57f1398922914bff73f0773be4c67
sobird <i@sobird.me>
2025-03-09 22:37:54 +0800

    chore: use react18 types

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import Sobird from './Sobird';
 
 const App = (props) => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <ul>
+        {new Array(5).fill(0).map((item, index) => {
+          return (
+            <li>
+              item,
+              {index}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 console.log('App', App);
